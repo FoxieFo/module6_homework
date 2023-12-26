f8630dbcd614946f07984a8dee3e858fd1a9cbc5
Victoria <151223562+FoxieFo@users.noreply.github.com>
2023-12-27 00:14:47 +0100

    implemented some changes

--- ex1.js
+++ ex1.js
@@ -1,18 +1,27 @@
 // Задание 1
 
-function getEvenOdd(userInput) {
-    let value = +userInput;
-  
-    if (!isNaN(value) && typeof value === 'number') {
-      if (value % 2 === 0) {
-        console.log('Ваше число четное');
-      } else {
-        console.log('Ваше число нечетное');
+const arrNums = [1, 2, 0, 5, 8, 0, 10, 7, 3];
+let evenCount = 0;
+let oddCount = 0;
+let zeroCount = 0;
+
+function countNumbers(arr) {
+
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] === 'number' && !isNaN(arr[i])) {
+        if (arr[i] === 0) {
+          zeroCount++;
+        } else if (arr[i] % 2 === 0) {
+          evenCount++;
+        } else {
+          oddCount++;
+        }
       }
-    } else {
-      console.log('Упс! Кажется, вы ошиблись');
     }
+  
+    console.log('Количество четных элементов:', evenCount);
+    console.log('Количество нечетных элементов:', oddCount);
+    console.log('Количество нулевых элементов:', zeroCount);
   }
 
-  const userInput = prompt('Введите число:');
-  getEvenOdd(userInput);
+  countNumbers(arrNums);
